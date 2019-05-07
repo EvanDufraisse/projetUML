46131aa0409a47d676a8cb07502ccb9e9e058866
Evan Dufraisse <edufraisse@gmail.com>
2019-05-07 09:53:15 +0200

    Oeuvre classs

--- src/projetUML/Oeuvre.java
+++ src/projetUML/Oeuvre.java
@@ -1,5 +1,68 @@
 package projetUML;
 
+import java.util.ArrayList;
+
 public abstract class  Oeuvre {
+	
+	long reference;
+	String titre;
+	String URL;
+	String type;
+	ArrayList<String> chapitres;
+	ArrayList<String>  domaines;
+	
+	public Oeuvre(long reference, String titre, String uRL) {
+		super();
+		this.reference = reference;
+		this.titre = titre;
+		URL = uRL;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public ArrayList<String> getChapitres() {
+		return chapitres;
+	}
+
+	public void setChapitres(ArrayList<String> chapitres) {
+		this.chapitres = chapitres;
+	}
+
+	public ArrayList<String> getDomaines() {
+		return domaines;
+	}
+
+	public void setDomaines(ArrayList<String> domaines) {
+		this.domaines = domaines;
+	}
+
+	public long getReference() {
+		return reference;
+	}
+	
+	
+	
 
 }
--- src/projetUML/Oeuvre_collective.java
+++ src/projetUML/Oeuvre_collective.java
@@ -2,4 +2,9 @@ package projetUML;
 
 public class Oeuvre_collective extends Oeuvre {
 
+	public Oeuvre_collective(long reference, String titre, String uRL) {
+		super(reference, titre, uRL);
+		// TODO Auto-generated constructor stub
+	}
+
 }
