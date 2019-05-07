4c0c28a1aae5fc59adbf31e7cf9eaf5416fbeacc
Evan Dufraisse <edufraisse@gmail.com>
2019-05-07 09:46:35 +0200

    Personne Class

--- src/projetUML/Personne.java
+++ src/projetUML/Personne.java
@@ -1,5 +1,51 @@
 package projetUML;
 
 public abstract class Personne {
+	String name;
+	String prenom;
+	String adresse;
+	int Id;
+	
+	public Personne(String name, String prenom, String adresse, int id) {
+		super();
+		this.name = name;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+	
+	
 
 }
