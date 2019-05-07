515eef5b5fa290abb72b8ca4cddd782d9aaf7389
Flo <florentcheneau@outlook.com>
2019-05-07 10:00:22 +0200

    Implémente Auteur

--- src/projetUML/Auteur.java
+++ src/projetUML/Auteur.java
@@ -1,5 +1,23 @@
 package projetUML;
 
+import java.util.ArrayList;
+
 public class Auteur extends Personne {
 
+	private ArrayList<String> domaines;
+
+	public Auteur(String name, String prenom, String adresse, int id, ArrayList<String> domaines) {
+		super(name, prenom, adresse, id);
+		this.domaines = domaines;
+	}
+	public Auteur(String name, String prenom, String adresse, int id) {
+		super(name, prenom, adresse, id);
+		this.domaines = new ArrayList<String>();
+	}
+	public ArrayList<String> getDomaines() {
+		return this.domaines;
+	}
+	public void addDomaine(String dom) {
+		this.getDomaines().add(dom);
+	}
 }
--- src/projetUML/Correcteur.java
+++ src/projetUML/Correcteur.java
@@ -1,5 +1,14 @@
 package projetUML;
 
+import java.util.ArrayList;
+
 public class Correcteur extends Personne{
+	
+	private ArrayList<String> domaines;
+	
+	public Correcteur(String name, String prenom, String adresse, int id) {
+		super(name, prenom, adresse, id);
+		
+	}
 
 }
